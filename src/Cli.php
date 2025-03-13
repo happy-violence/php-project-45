bea6db4644ceb798956de792e7edfff0d82336d3
Svetlana Makarova <CBETA-MAKAPOBA@yandex.ru>
2025-03-13 21:32:48 +0300

    fix an error in  function greeting

--- src/Cli.php
+++ src/Cli.php
@@ -3,9 +3,9 @@ namespace Src\Cli;
 
 use function cli\line;
 use function cli\prompt;
-function greeting($name)
+function greeting()
 {
     line('Welcome to the Brain Game!');
-    prompt('May I have your name?');
+    $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
