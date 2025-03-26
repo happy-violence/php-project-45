4324692492dba7adc12d29cb651316e4d2dabce2
Svetlana Makarova <CBETA-MAKAPOBA@yandex.ru>
2025-03-26 21:39:37 +0300

    make step 6

---
+++ src/Engine.php
@@ -0,0 +1,31 @@
+<?php
+namespace Src\Engine;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+
+
+function startGame(string $info): void
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+
+    line($info);
+    $count = 0;
+    while($count < 3) {
+
+        [$userAnswer, $correctAnswer] = logic();
+        //сверяем ответ игрока с правильным
+        if($userAnswer == $correctAnswer) {
+            line('Correct!');
+            $count++;
+        } else {
+            line($userAnswer . ' is wrong answer ;(. Correct answer was ' . $correctAnswer . '. Let\'s try again, ' . $name . '!');
+            $count = 0;
+        }
+    }
+    line('Congratulations, ' . $name . '!');
+}
