2bf12e352c9e532989435804415156385ba691fb
Svetlana Makarova <CBETA-MAKAPOBA@yandex.ru>
2025-04-17 15:48:30 +0300

    fixed the transfer of variables in function gcd

--- src/Games/gcd.php
+++ src/Games/gcd.php
@@ -13,10 +13,8 @@ function gcd(int $num1, int $num2): int
     if ($num2 === 0) {
         return $num1;
     }
-    $temp = $num2;
-    $num2 = $num1 % $num2;
-    $num1 = $temp;
-    return gcd($num1, $num2);
+
+    return gcd($num2, $num1 % $num2);
 }
 
 function runGcd(): void
